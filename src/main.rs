857bfee84a13d9a379d44be0c92eed7885a10d59
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-01-26 11:19:27 +0100

    added clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "container")]
+#[command(about = "A simple container runtime")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(clap::Subcommand)]
+enum Commands {
+    Run {
+        rootfs: String,
+        command: String,
+        #[arg(trailing_var_arg = true)]
+        args: Vec<String>,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Run { rootfs, command, args } => {
+            println!("Running {} in {}", command, rootfs);
+        }
+    }
 }
